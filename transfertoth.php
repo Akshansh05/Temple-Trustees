397f9ed2436062caa2aeca1d236e91730321e69e
Akshansh05 <akshanshohm1999@gmail.com>
2018-04-13 02:47:01 +0530

    Update transfertoth.php

--- transfertoth.php
+++ transfertoth.php
@@ -42,8 +42,8 @@ $new_initial_amount=$value-$t_amount;
 			return false;
 		}
     }
-    $met3="Amount after transfer to other the Account";
-    $met4="Amount after receiving from other Account";
+    $met3="Amount after transfer to ".$d_email ." the Account";
+    $met4="Amount after receiving from ". $email. "Account";
 $sql2 = "UPDATE transfer SET amount='$new_initial_amount', date='$today',method='$met3' WHERE trustname='$initial_product'and buyer='$email'";
     $result2 = mysqli_query($conn,$sql2);
     if($new_initial_amount==0)
